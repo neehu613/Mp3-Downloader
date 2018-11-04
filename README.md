4aac0cc879dff2003ac4438b7fe7bc3168f12750
neehu613 <41267809+neehu613@users.noreply.github.com>
2018-11-04 12:14:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# mp3-downloader
+# Mp3-Downloader
+Enter a song, sit back, relax and let python do it's work.
+
+Enter the song name/artist name. You are presented with a list of matching songs. Select the song. Download will start automatically.
---
+++ mp3Downloader.py
@@ -0,0 +1,54 @@
+import urllib2, re, webbrowser, os
+from bs4 import BeautifulSoup
+
+os.system("clear")
+url = 'http://en.muzmo.ru/search?q='
+song = raw_input("Enter the song/artist name: ")
+song = re.sub(' ', '+', song)
+url += song
+page = urllib2.urlopen(url)
+soup = BeautifulSoup(page, "lxml")
+allLinks = soup.find_all('a', class_='block')
+songList = list()
+songs = list()
+songQuality = list()
+songLinks = list()
+num=1
+for item in allLinks:
+	link = item.get("href")
+	checkIfSong = link[0:4]
+	if checkIfSong == "/get":
+		songList.append("http://en.muzmo.ru" + link)
+		song = re.sub('  ', '', item.text)
+		songs.append(str(num) + " " + song)
+		num+=1
+		
+for song in songs:
+	print song
+
+choice = int(input("Enter your choice: "))
+if choice > num or choice <= 0:
+	print "Invalid Choice"
+else:
+	print "Song selected = " + str(songs[choice-1])
+
+url = str(songList[choice-1])
+page = urllib2.urlopen(url)
+soup = BeautifulSoup(page, "lxml")
+allLinks = soup.find_all('a', class_='block')
+
+num=1
+for link in allLinks:
+	song = link.get("href")
+	songQ = song[0:20]
+	if songQ == "http://muzmo.ru/get/":
+		songQuality.append(str(num) + " " + str(link.text))
+		songLinks.append(song)
+		num += 1
+
+for quality in songQuality:
+	quality = re.sub("  ", "", quality)
+	print quality
+
+quality = int(input("Select the quality: "))
+webbrowser.open_new(songLinks[quality-1])
